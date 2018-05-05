984e149844d5acba83618c0eb9134bfd0087c018
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2018-05-05 21:20:45 +1000

    create Heuristic objects and use them to hold the descrption. expand the descriptions

--- backend/evaluate/heuristics/core.py
+++ backend/evaluate/heuristics/core.py
@@ -1,6 +1,44 @@
 from enum import Enum
 
 
+class Heuristic():
+    def __init__(self, key, motivator, description):
+        self.key = key
+        self.motivator = motivator
+        self.description = description
+
+
+ALL_HEURISTICS = [
+    Heuristic(
+        'trust-the-experts',
+        'I trust in experts',
+        'In order to gain the best information on topics such as economics, politics, philosophy, biology etc turn to the experts on those fields, who dedicate themselves to specialising in complex problem domains. So in order honor this information-quality heuristic, we evaluate publication on the basis of how many academics contribute to the publication. Use this heuristic is you care that a publishers has experts contributing to their publication, and not amateurs.'
+    ),
+    Heuristic(
+        'is-pro-science',
+        'I want science backed information',
+        'Science is perhaps the greatest knowledge seeking tool of mankind. Often where scientific knowledge exists it is ignored or misused by content publishers. Use this heuristic if you are most interested in sources that put a strong emphasis on respect of scientific expertise and the scientific method.'
+    ),
+    Heuristic(
+        'has-subscription-revenue',
+        'I don\'t want click-funded content',
+        'This heuristic asks if a publication has a subscription revenue model. Purely click-funded publications have mis-aligned incentives with users. They are motivated to sensationalise, mislead and fabricate in order to drive \'clicks\'. Publications that have a subscription model are not as incentivised to drive clicks, and instead seek to gain the respect and loyalty of their paying subscribers.'
+    ),
+    Heuristic(
+        'bias',
+        'I want to avoid highly biased sources',
+        'Whether left or right, political bias has a tendency distort information reporting away from factual and towards ideological. Do note however, that high bias is not necessarily an negative. To assume so is to commit the [*Argument to Moderation*](https://en.wikipedia.org/wiki/Argument_to_moderation) fallacy'
+    ),
+    Heuristic(
+        'i-value-sources',
+        'I value sources',
+        'Good information is typically *dense* information. A lack of links to external websites is a signal of shallow and/or facile analyis.'
+    ),
+]
+
+HEURISTICS_DICT = {x.key: x for x in ALL_HEURISTICS}
+
+
 class HeuristicID(Enum):
     TRUST_EXPERTS = 1
     HAS_SUBSCRIPTION_MODEL = 2
--- backend/resources/heuristics.py
+++ backend/resources/heuristics.py
@@ -2,29 +2,13 @@ import json
 from flask import Response
 from flask_restful import Resource, reqparse
 
+from backend.evaluate.heuristics.core import ALL_HEURISTICS
+
+
 class Heuristics(Resource):
     def get(self):
         heuristics = [
-            {
-                'name': 'I trust the experts',
-                'description': 'How many academics contribute to the publication?'
-            },
-            {
-                'name': 'I value sources',
-                'description': 'How many links out of an average article on the publication?'
-            },
-            {
-                'name': 'I don\'t want click-funded content',
-                'description': 'Do they have a subscription revenue model?'
-            },
-            {
-                'name': 'I want to avoid dishonest publications',
-                'description': 'How many time have their articles been review as \"false\" on Snopes.com'
-            },
-            {
-                'name': 'I want to avoid highly biased sources',
-                'description': 'Use https://mediabiasfactcheck.com/'
-            }
+            {'name': x.motivator, 'description': x.description} for x in ALL_HEURISTICS
         ]
 
         return heuristics
