03e300ffa15c5aa4f80bc60303cac71d6dfdd7b6
zcallan <kaln.delbridge@hotmail.com>
2018-05-06 02:09:00 +1000

    updated heuristics reducer to store as key value

--- frontend/src/utils/methodology/heuristics/methodologyHeuristics.actions.js
+++ frontend/src/utils/methodology/heuristics/methodologyHeuristics.actions.js
@@ -1,10 +1,49 @@
-import { Api } from '../../index';
+// import { Api } from '../../index';
 
 export const fetchMethodologyHeuristics = () => {
   return async dispatch => {
     dispatch({
       type: 'FETCH_METHODOLOGY_HEURISTICS_REQUEST',
     });
+
+    dispatch({
+      type: 'FETCH_METHODOLOGY_HEURISTICS_SUCCESS',
+      payload: [
+        {
+          name: 'I trust in experts',
+          description: 'In order to gain the best information on topics such as economics, politics, philosophy, biology etc turn to the experts on those fields, who dedicate themselves to specialising in complex problem domains. So in order honor this information-quality heuristic, we evaluate publication on the basis of how many academics contribute to the publication. Use this heuristic is you care that a publishers has experts contributing to their publication, and not amateurs.',
+          key: 'trust-the-experts',
+          id: 1,
+        },
+        {
+          name: 'I want science backed information',
+          description: 'Science is perhaps the greatest knowledge seeking tool of mankind. Often where scientific knowledge exists it is ignored or misused by content publishers. Use this heuristic if you are most interested in sources that put a strong emphasis on respect of scientific expertise and the scientific method.',
+          key: 'is-pro-science',
+          id: 3,
+        },
+        {
+          name: 'I don\'t want click-funded content',
+          description: 'This heuristic asks if a publication has a subscription revenue model. Purely click-funded publications have mis-aligned incentives with users. They are motivated to sensationalise, mislead and fabricate in order to drive \'clicks\'. Publications that have a subscription model are not as incentivised to drive clicks, and instead seek to gain the respect and loyalty of their paying subscribers.',
+          key: 'has-subscription-revenue',
+          id: 2,
+        },
+        {
+          name: 'I want to avoid highly biased sources',
+          description: 'Whether left or right, political bias has a tendency distort information reporting away from factual and towards ideological. Do note however, that high bias is not necessarily an negative. To assume so is to commit the [*Argument to Moderation*](https://en.wikipedia.org/wiki/Argument_to_moderation) fallacy',
+          key: 'bias',
+          id: 4,
+        },
+        {
+          name: 'I value sources',
+          description: 'Good information is typically *dense* information. A lack of links to external websites is a signal of shallow and/or facile analyis.',
+          key: 'i-value-sources',
+          id: 5,
+        },
+      ],
+    });
+
+    /*
+    FIXME: make this actually fetch data when CORS is fixed
 
     try {
       const { data } = await Api.getMethodologyHeuristics();
@@ -18,6 +57,7 @@ export const fetchMethodologyHeuristics = () => {
         fetchMethodologyHeuristicsFailure( error )
       );
     }
+    */
   };
 };
 
--- frontend/src/views/pages/home/Home.js
+++ frontend/src/views/pages/home/Home.js
@@ -1,47 +1,15 @@
 import React, { Component } from 'react';
-import { func } from 'prop-types';
+import { func, object } from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { fetchMethodologyHeuristics } from 'flux/actions';
 import Layout from 'views/layout';
 import { Heading, Box, Dropdown } from 'views/components';
 
-const data = [
-  {
-    name: 'I trust in experts',
-    description: 'In order to gain the best information on topics such as economics, politics, philosophy, biology etc turn to the experts on those fields, who dedicate themselves to specialising in complex problem domains. So in order honor this information-quality heuristic, we evaluate publication on the basis of how many academics contribute to the publication. Use this heuristic is you care that a publishers has experts contributing to their publication, and not amateurs.',
-    key: 'trust-the-experts',
-    id: 1,
-  },
-  {
-    name: 'I want science backed information',
-    description: 'Science is perhaps the greatest knowledge seeking tool of mankind. Often where scientific knowledge exists it is ignored or misused by content publishers. Use this heuristic if you are most interested in sources that put a strong emphasis on respect of scientific expertise and the scientific method.',
-    key: 'is-pro-science',
-    id: 3,
-  },
-  {
-    name: 'I don\'t want click-funded content',
-    description: 'This heuristic asks if a publication has a subscription revenue model. Purely click-funded publications have mis-aligned incentives with users. They are motivated to sensationalise, mislead and fabricate in order to drive \'clicks\'. Publications that have a subscription model are not as incentivised to drive clicks, and instead seek to gain the respect and loyalty of their paying subscribers.',
-    key: 'has-subscription-revenue',
-    id: 2,
-  },
-  {
-    name: 'I want to avoid highly biased sources',
-    description: 'Whether left or right, political bias has a tendency distort information reporting away from factual and towards ideological. Do note however, that high bias is not necessarily an negative. To assume so is to commit the [*Argument to Moderation*](https://en.wikipedia.org/wiki/Argument_to_moderation) fallacy',
-    key: 'bias',
-    id: 4,
-  },
-  {
-    name: 'I value sources',
-    description: 'Good information is typically *dense* information. A lack of links to external websites is a signal of shallow and/or facile analyis.',
-    key: 'i-value-sources',
-    id: 5,
-  },
-];
-
 class Home extends Component {
   static propTypes = {
     fetchMethodologyHeuristics: func,
+    methodology: object,
   }
 
   componentDidMount() {
@@ -53,6 +21,12 @@ class Home extends Component {
   }
 
   render() {
+    const { heuristics } = this.props.methodology;
+    const heuristicsData = (
+      heuristics.fetched &&
+      Object.keys( heuristics.data ).map( heuristic => heuristics.data[heuristic] )
+    );
+
     return (
       <Layout
         backgroundWaves
@@ -74,18 +48,29 @@ class Home extends Component {
               Choose a news quality signal
             </Heading>
 
-            <Dropdown items={data} onChange={this.handleChange}>
+            <Dropdown items={heuristics.data} onChange={this.handleChange}>
               {({ selectedIndex, onSelect, isOpen, onToggle }) => (
                 <div>
                   <button
                     onClick={onToggle}
                   >
-                    {data[selectedIndex].name}
+                    {(
+                      heuristicsData &&
+                      heuristicsData.length > 0
+                    )
+                      ? heuristicsData[selectedIndex].name
+                      : heuristics.fetching
+                        ? 'Loading...'
+                        : 'Failed to load heuristics.'}
                   </button>
 
-                  {isOpen && (
+                  {(
+                    isOpen &&
+                    heuristicsData &&
+                    heuristicsData.length > 0
+                  ) && (
                     <ul>
-                      {data.map(( item, index ) => (
+                      {heuristicsData.map(( item, index ) => (
                         <li
                           key={item.id}
                           onClick={onSelect( index )}
