6b2322f86ec346f946359e6e482c56002e6e42d4
Michael Swiatkiwsky <michael@swiatkiwsky.com>
2018-05-05 22:44:53 +1000

    Added else statement for avoid highly biased

--- backend/evaluate/heuristics/avoid_highly_biased.py
+++ backend/evaluate/heuristics/avoid_highly_biased.py
@@ -5,6 +5,9 @@ def score_publication(publication):
     if publication.bias_level == 0:
         return HeuristicScore.max()
     elif publication.bias_level == 1:
-        return 5
+        return HeuristicScore(5)
     elif publication.bias_level == 2:
         return HeuristicScore.min()
+    else:
+        # Not penalizing if no bias level provided
+        return HeuristicScore.max()
