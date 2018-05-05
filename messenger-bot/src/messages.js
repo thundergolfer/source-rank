b9fc014580bc337cc624b4daaaf62fcd3cd5c7a2
matthaywardwebdesign <matt@matthayward.com.au>
2018-05-05 15:00:40 +1000

    Added URL parsing and listening for introduction messages

--- messenger-bot/src/messages.js
+++ messenger-bot/src/messages.js
@@ -1,17 +1,46 @@
 /* Include dependencies */
 import logger from './logger';
 import FB from './fb';
+import getURLs from 'get-urls';
+import exampleData from './example/example-data';
+
+/* Define a list of messages that trigger an introduction */
+const INTRODUCTION_TRIGGER_PHRASES = [ 'hey', 'hi', 'help', 'how', '?' ];
 
 class Messages {
   processMessage( data ) {
     /* Get all of the key info */
-    const { sender, timestamp, message } = data;
+    const { sender, message } = data;
 
     /* Log the received message */
     logger.info( `Message from ${sender.id} - ${message.text}` );
 
-    /* Test sending a message back */
-    FB.sendMessage( sender.id, 'Thanks for trying out the bot!' );
+    /* Get the message text content */
+    let { text } = message;
+
+    /* Trim the message */
+    text = text.trim();
+
+    /* Try and parse any URLs in the string */
+    const urls = getURLs( text );
+
+    /* If we've got some URLs tell the user that we are processing them */
+    if ( urls.size ) {
+      FB.sendMessage( sender.id, `Hold tight! We are ranking ${urls.length > 1 ? 'all the links you sent us now' : 'the link'} now, we'll be back in a sec.` );
+      return;
+    }
+
+    /* We couldn't find any URLs, check for whether the text contains any of the introduction trigger phrases */
+    for ( let i = 0; i < INTRODUCTION_TRIGGER_PHRASES.length; i++ ) {
+      if ( text.toLowerCase().includes( INTRODUCTION_TRIGGER_PHRASES[i] )) {
+        /* Send back the introduction */
+        FB.sendMessage( sender.id, 'Hey there! I\'m SourceRank!\nI\'m pretty simple to use. Just send me a link and I\'ll give it a ranking. Easy as right?' );
+        return;
+      }
+    }
+
+    /* Message isn't a URL. Send back a message containing some instructions */
+    FB.sendMessage( sender.id, 'Hmm... that doesn\'t look like a link. To use SourceRank simply send us a URL and we\'ll rank it for you!' );
   }
 }
 
